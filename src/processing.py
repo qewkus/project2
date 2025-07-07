52c60104202e53bff96dba5f23e92c2f85c9aaf5
Алексей <denaz2020@mail.ru>
2025-07-08 01:39:13 +0800

    add filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,16 @@
+operations = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+ {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+  {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+ {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+def filter_by_state(operations: list , state: str ='EXECUTED') -> list:
+    """Возвращает новый список словарей, у которых ключ соответствует значению."""
+    filter = []
+    for operation in operations:
+        if operation.get('state') == state:
+            filter.append(operation)
+    return filter
+
+
+print(filter_by_state(operations, state='CANCELED'))
