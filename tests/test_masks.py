4b0ce8e4452bf0c81a53a8dfd815197f037883da
Алексей <denaz2020@mail.ru>
2025-07-16 01:57:23 +0800

    tested by tests for linters

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,45 +1,36 @@
 import pytest
+
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def test_get_mask_account():
+def test_get_mask_account() -> None:
     assert get_mask_account("73654108430135874305") == "**4305"
 
 
-def test_get_mask_account_longer():
+def test_get_mask_account_longer() -> None:
     with pytest.raises(ValueError):
         get_mask_account("736541084301358743052354")
 
 
-def test_get_mask_account_shorted():
+def test_get_mask_account_shorted() -> None:
     with pytest.raises(ValueError):
         get_mask_account("736541084301")
 
 
-def test_get_mask_card_number():
+def test_get_mask_card_number() -> None:
     assert get_mask_card_number("7000792289606361") == "7000 79** ****6361"
 
 
-def test_get_mask_card_number_longer():
+def test_get_mask_card_number_longer() -> None:
     with pytest.raises(ValueError):
         get_mask_card_number("736541084301358743052354")
 
 
-def test_get_mask_card_number_empty():
+def test_get_mask_card_number_empty() -> None:
     with pytest.raises(ValueError):
         get_mask_card_number("")
 
 
-def test_get_mask_card_number_shorted():
+def test_get_mask_card_number_shorted() -> None:
     with pytest.raises(ValueError):
         get_mask_card_number("736541084301")
-
-
-
-
-
-
-
-
-
-
